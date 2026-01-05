9eeb962a9d764d6ffd90478733eb9dc3b8705a90
“Tengereeb8” <tengererdenebattulga8@gmail.com>
2026-01-05 13:26:37 +0800

    done

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,65 +1,86 @@
-import Image from "next/image";
+// import { imageConfigDefault } from "next/dist/shared/lib/image-config";
 
+// export default function Home() {
+//   return (
+//     <div className="bg-pink-300 min-h-screen flex justify-center items-center">
+//       <BirthdayCard />
+//     </div>
+//   );
+// }
+// const birthday = [
+//   { id: 1, image: "https://picsum.photos/200", name: "Bat", age: 25 },
+// ];
+// const BirthdayCard = () => {
+//   return (
+//     <div className="w-150 h-170 bg-white text-black pl-20 ">
+//       <h1 className="mt-8 text-[1.953rem]">5 Birthdays today</h1>
+//       <div>
+//         {birthday.map(({ id, name, age, image }) => (
+//           <BirthdayCardItem key={id} name={name} age={age} image={image} />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+// const BirthdayCardItem = () => {
+//   return (
+//     <div className="flex gap-8">
+//       <img src="image" alt="" />
+//       <div className="flex flex-col justify-center gap-3">
+//         <h1>{name}</h1>
+//         <p>{age}</p>
+//       </div>
+//     </div>
+//   );
+// };
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="bg-pink-300 min-h-screen flex justify-center items-center">
+      <BirthdayCard />
     </div>
   );
 }
+
+const birthday = [
+  { id: 1, image: "https://picsum.photos/200", name: "Bat", age: 22 },
+
+  { id: 2, image: "https://picsum.photos/200", name: "Dorj", age: 19 },
+
+  { id: 3, image: "https://picsum.photos/200", name: "Baldan", age: 35 },
+
+  { id: 4, image: "https://picsum.photos/200", name: "Hulan", age: 21 },
+
+  { id: 5, image: "https://picsum.photos/200", name: "Nomin", age: 20 },
+];
+
+const BirthdayCard = () => {
+  return (
+    <div className="w-150  bg-white text-black pl-20">
+      <h1 className="mt-8 text-[1.953rem]">
+        {birthday.length} Birthdays today
+      </h1>
+
+      <div className="mt-6 flex flex-col gap-6 mb-6">
+        {birthday.map(({ id, name, age, image }) => (
+          <BirthdayCardItem key={id} name={name} age={age} image={image} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const BirthdayCardItem = ({ name, age, image }) => {
+  return (
+    <div className="flex gap-8 items-center">
+      <img
+        src={image}
+        alt={name}
+        className="w-20 h-20 rounded-full object-cover"
+      />
+      <div className="flex flex-col">
+        <h1 className="font-semibold">{name}</h1>
+        <p className="text-gray-500">{age} years</p>
+      </div>
+    </div>
+  );
+};
